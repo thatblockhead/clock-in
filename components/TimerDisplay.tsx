f21c9966a56185e124a5bbeffe03efc76ea82d0c
xamdel <xamdel@protonmail.com>
2023-09-22 20:32:40 -0400

    create TimerDisplay component

---
+++ components/TimerDisplay.tsx
@@ -0,0 +1,15 @@
+type TimerDisplayProps = {
+    time: number;
+};
+
+export const TimerDisplay: React.FC<TimerDisplayProps> = ({ time }) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+    return <h1>{formattedTime}</h1>;
+};  
